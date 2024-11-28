c86662d35feaa889f97fa41ecc9310ff12005765
Arpit-090 <232010@kit.ac.in>
2024-11-28 15:30:44 +0000

    container configure

---
+++ .devcontainer/java/2ndjs.js
@@ -0,0 +1,6 @@
+//type conversion
+let code = "09998"
+console.log(typeof(code))
+let code2 = Number(code)
+console.log(typeof(code2))
+console.log(typeof(code))
--- .devcontainer/java/test.js
+++ .devcontainer/java/test.js
@@ -1,1 +1,17 @@
+"use strict"   //used for treating our code by newest version
+//alert("arpit") //can't use on nodejs but use in browser
 console.log("arpit")
+const id="1234"   //declaring constants
+let nam="arpit"    //dec variables
+var clas="nothing"    //also varible
+college="KIT";
+console.log(id)  //printing values
+console.table([id ,nam, clas,college ]) //printing in table
+// we use let instead of var because problem of block scope
+console.log(typeof "nothing")
+console.log(typeof null)
+console.log(typeof undefined)
+/*
+data types in js = int , big int , string , object , boolean ;
+
+
