56833921691e6005efc185a9674ead5ef169b952
Arpit-090 <232010@kit.ac.in>
2024-11-28 19:45:52 +0000

    container configure

--- .devcontainer/java/2ndjs.js
+++ .devcontainer/java/2ndjs.js
@@ -1,6 +1,36 @@
 //type conversion
-let code = "09998"
+let code = 9998
 console.log(typeof(code))
-let code2 = Number(code)
-console.log(typeof(code2))
+let code2 = String(code)   //use capital latter for conversion like String,Number;
+console.log(typeof(code2)) //is also gives NaN when a string converted to a number which is actually not a number
 console.log(typeof(code))
+console.log("2"+1)  // js auto convert number to string and give 21
+console.log("2"+1+1)
+console.log(2+1+"1")  // cant convert the last string to number
+//comparision operator
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+// strict check ===
+console.log("2"==2);
+console.log("2"===2);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// memory allocation
+//stack (primitive data types)  and head (non primitive data type)
+let name1 = "arpit"
+let name2=name1;
+name2="kumar"
+console.log(name2)
+console.log(name1) 
+//for non primitive
+
+let  obj= {
+     mail : "232010@",
+    Upi:"995630"
+}
+
+//let obj2 = Obj
+//my_obj2.mail="231940@"
+console.log(obj.mail);
+//console.log(obj2.mail);
+
--- .devcontainer/java/test.js
+++ .devcontainer/java/test.js
@@ -4,14 +4,40 @@ console.log("arpit")
 const id="1234"   //declaring constants
 let nam="arpit"    //dec variables
 var clas="nothing"    //also varible
-college="KIT";
+let college="KIT";
 console.log(id)  //printing values
 console.table([id ,nam, clas,college ]) //printing in table
 // we use let instead of var because problem of block scope
 console.log(typeof "nothing")
 console.log(typeof null)
 console.log(typeof undefined)
-/*
-data types in js = int , big int , string , object , boolean ;
 
+//data types in js     primitive datatype(call by value) = int , big int , string , null,undefined,symbol , boolean ;
+//                   non primitive(call by referance)= array,objects,functions
 
+// It store string data type
+let txt = "GeeksforGeeks";
+
+// It store integer data type
+let a = 5;
+let b = 5;
+
+// It store Boolean data type
+(a == b )
+
+// To check Strictly (i.e. Whether the datatypes
+// of both variables are same) === is used
+//(a === b)---> returns true to the console
+ 
+// It store array data type
+let places= ["GFG", "Computer", "Hello"];
+
+// It store object data (objects are 
+// represented in the below way mainly)
+let Student = {
+    firstName: "Johnny",
+    lastName: "Diaz", 
+    age: 35, 
+    mark: "blueEYE"
+}
+console.log(student)
